1902fb9f4fad8d92841d0366a1a311ac1c3e4937
improvCoder <sandeepdeoja@gmail.com>
2025-06-10 13:48:36 -0700

    no training

--- hello.py
+++ hello.py
@@ -1,11 +1,25 @@
 import torch
 import matplotlib.pyplot as mt
+import math
 
-X=torch.rand(100)*10
-y=torch.rand(100)*10
-print(X)
-print(y)
-mt.scatter(X,y,marker='o')
+
+value_1=0.5
+value_2=2
+
+variable_1=torch.tensor([2,5,7])
+variable_2=torch.tensor([3.5,4.49,5.51])
+
+r=(variable_1*value_1) + value_2
+result=torch.tensor(r)
+
+print(variable_1)
+print(result)
+
+length=variable_1.size()
+
+err= math.sqrt(((r[0]-variable_2[0])**2/length[0])+((r[1]-variable_2[1])**2/length[0])+((r[2]-variable_2[2])**2/length[0]))
+
+print("Err:",err)
+
+mt.scatter(variable_1,result,marker='o')
 mt.show()
-
-
